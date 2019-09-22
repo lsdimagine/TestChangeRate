dc9d25f426f6ffeea18e896d2e59bea330149cd7
Shidong Lin <linshidongbqwl@gmail.com>
2019-09-21 18:12:58 -0700

    Initial Commit

---
+++ TestChangeRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestChangeRate
+//
+//  Created by Shidong Lin on 9/21/19.
+//  Copyright © 2019 Shidong Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
